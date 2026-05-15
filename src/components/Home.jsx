3cadf76cebc5abd97c77f9e7b3036f4c797358ba
Quyết <vmquyet2005@gmail.com>
2026-05-15 15:42:25 +0700

    Fix unused axios import in Home.jsx to resolve Vercel build error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col, Modal, Button } from 'react-bootstrap';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { 
     FiArrowRight, 
